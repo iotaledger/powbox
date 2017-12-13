feb608afe06a739eee47faef108551c71d9ddc64
Edward Greve <edward.greve@gmail.com>
2017-12-12 20:12:52 -0500

    Add job progress and completion updates to mongodb

---
+++ src/server/rabbit.js
@@ -0,0 +1,35 @@
+const amqp = require('amqplib');
+
+const { finishJob, updateJob } = require('../common/db');
+
+const { BROKER_URL, COMPLETED_QUEUE, PROGRESS_QUEUE } = process.env;
+
+module.exports.listen = async () => {
+    const conn = await amqp.connect(BROKER_URL);
+
+    const channel = await conn.createChannel();
+
+    await channel.assertQueue(COMPLETED_QUEUE);
+
+    channel.consume(COMPLETED_QUEUE, async msg => {
+        console.log(`[${COMPLETED_QUEUE}] received message ${msg.properties.messageId}`);
+
+        await finishJob(msg.properties.messageId, msg.content.toString());
+
+        channel.ack(msg);
+    });
+
+    console.log(`[${COMPLETED_QUEUE}] listening for messages`);
+
+    await channel.assertQueue(PROGRESS_QUEUE);
+
+    channel.consume(PROGRESS_QUEUE, async msg => {
+        console.log(`[${PROGRESS_QUEUE}] received message ${msg.properties.messageId}`);
+
+        await updateJob(msg.properties.messageId, msg.content.toString());
+
+        channel.ack(msg);
+    });
+
+    console.log(`[${PROGRESS_QUEUE}] listening for messages`);
+};
